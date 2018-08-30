b9e0991f67fc78370f956bd8917b7c253668b26d
Devon Mackay <devon@dds.mil>
2018-08-30 16:43:58 -0400

    Add only set env var if it does not exist

--- get_db_settings_functions.inc.sh
+++ get_db_settings_functions.inc.sh
@@ -4,9 +4,12 @@ set_db_env_vars () {
     local config_file="${1}"
 
     # Read in and export all name=value pairs where the name starts with PG
+    # unless we already have an env var with that value
     # (NOTE: all whitespaces are removed from each line)
     while read -r DBVAR
     do
-        eval "export ${DBVAR}"
+	if ! $(env | grep -qFle "${foo}"); then
+            eval "export ${DBVAR}"
+	fi
     done < <(grep -Ee '^PG' "${config_file}" | sed 's/ //g')
 }
